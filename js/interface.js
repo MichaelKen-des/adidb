ede579f90470b0757b7889e35ce83bc5d56db740
soloexitus <soloexitus@gmail.com>
2020-05-07 14:33:31 +0300

    final change

--- js/interface.js
+++ js/interface.js
@@ -13,7 +13,7 @@ class myinterface{
 
         document.getElementById('display').addEventListener('click', (x)=>{
             event.preventDefault();
-            if (!this.done){
+           if (!this.done){
             let data={
                 labels:[],
                 value:[],
@@ -22,7 +22,7 @@ class myinterface{
            GenerateData(data);
            this.display(data);
            this.done=1;
-            }
+          }
         } 
         );
     }
@@ -40,22 +40,25 @@ class myinterface{
     }
 
     display(data){
-        document.getElementById('analit').classList.remove('invis');
-        let ctx = document.getElementById('analit').getContext('2d');
-
-        this.myBarChart = new Chart( ctx, {
+        //document.getElementById('analit').classList.remove('invis');
+        //let ctx = document.getElementById('analit').getContext('2d');
+        let ctx;
+        ctx=document.createElement('canvas');
+        ctx.id='analit';
+        this.myBarChart = new Chart( ctx.getContext('2d'), {
         type: 'doughnut',
         data: {
             labels:data.labels,
             datasets: [{
                 label:"Hobby",
                 backgroundColor:data.color,
-                borderColor: 'rgb(100,130,132)',
+                borderColor: 'rgb(255,255,255)',
                 data:data.value,
             }]
         },
         //options: options
     });
+    document.getElementById("graphic").append(ctx);
     }
 
 }
